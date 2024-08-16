16b7318545090a0ede22b2032a5e175ab6a118e8
o-d1 <oscar.davies109@gmail.com>
2024-08-16 23:33:54 +0100

    Saving changes

--- akr v2/ark-v2.js
+++ akr v2/ark-v2.js
@@ -1,1 +1,18 @@
 // Search functionality
+
+function findDinos () {
+    // Getting elements from the DOM
+    let searchBar = document.getElementById("searchBar").value.toUpperCase();
+    let dinos = document.getElementById("dinos");
+    let info = dinos.getElementsByTagName("tr");
+
+    for (i = 0; i < info.length; i++) {
+        let column = info[i].getElementsByTagName("td")[0];
+        let language = column.textContent;
+    
+        rows[i].style.display =
+          language.toUpperCase().indexOf(searchBar) > -1 ? "" : "none";
+      }
+    }
+    
+document.getElementById("searchInput").addEventListener("keyup", findDinos)
